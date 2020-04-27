426547a5d62e117a104f765a26eec103b4f19b69
Giosuè <873810@stud.unive.it>
2020-04-27 18:40:13 +0200

    update table
    update model.py

--- app/model.py
+++ app/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column,  Integer, String
+from sqlalchemy import Table, Column, Integer, String, Float
 from app import metadata, engine
 #--------tabella
 users = Table( 'users', metadata,
@@ -7,5 +7,23 @@ users = Table( 'users', metadata,
     Column('email', String, unique = True),
     Column('password', String)
 )
+theater = Table('theater', metadata,
+             Column('id', Integer, primary_key = True),
+             Column('placesAvailable', Integer, nullable = False))
+
+movie = Table('movie', metadata,
+             Column('title', String),
+             Column('minimumAge', Integer, default = 0),
+             Column('duration', Float, nullable = False),
+             Column('id', Integer))
+
+genreMovie = Table('genreMovie', metadata,
+                   Column('idMovie', None, primary_key = True, ForeignKey('movie.id')),
+                   Column('idGenre', None, primary_key = True, ForeignKey('genre.id')))
+genre = Table('genre', metadata,
+               Column('id', Integer, primary_key = True),
+               Column('description', String))
+
+
 metadata.create_all(engine)
 #----------fine tabella
