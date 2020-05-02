2e60af35b39001a2b7cc125e0499e28f0e7b0b62
Giorgio <874446@stud.unive.it>
2020-05-02 16:25:00 +0200

    New table booking

--- app/model.py
+++ app/model.py
@@ -6,31 +6,31 @@ users = Table( 'users', metadata,
             Column('name', String),
             Column('surname', String),
             Column('email', String, unique = True, nullable = False),
-            Column('password', String)
+            Column('password', String, nullable = False)#PENSARE MINIMO LUNGHEZZE
         )
 
 clients = Table( 'clients', metadata,
                 Column('id', None , ForeignKey('users.id'), primary_key = True),
-                Column('credit', Float),
+                Column('credit', Float),#credito potrebbe essere not null e >=0
                 Column('birthDate', Date)#, CheckConstraint( (date.today() - 'birthDate').days > 0 )),
             )
 
 managers = Table('managers', metadata,
                 Column('id', None , ForeignKey('users.id'), primary_key = True),
                 Column('admin', Float, nullable = False),
-                Column('financialReport', Float)
+                Column('financialReport', Float)#>=0 not null
             )
 
 theaters = Table('theaters', metadata,
              Column('id', Integer, primary_key = True),
-             Column('placesAvailable', Integer, nullable = False)
+             Column('seatsCapacity', Integer, nullable = False)#numero posti >=0
             )
 
 movies = Table('movies', metadata,
              Column('id', Integer, primary_key = True), 
-             Column('title', String),
-             Column('minimumAge', Integer, default = 0),
-             Column('duration', Float, nullable = False),
+             Column('title', String),#not null
+             Column('minimumAge', Integer, default = 0),#CHECK >=0
+             Column('duration', Float, nullable = False),#CHECK >=0
 
             )
 
@@ -41,19 +41,25 @@ genreMovies = Table('genreMovies', metadata,
 
 genres = Table('genres', metadata,
                Column('id', Integer, primary_key = True),
-               Column('description', String)
+               Column('description', String)#NOT NULL
             )
 
-programming = Table('programming', metadata,
-                Column('dateTime',DateTime),
-                Column('price', Float),
-                Column('theater', None, ForeignKey('theaters.id')),
+movieShedule = Table('movieShedule', metadata,        #RIPENSARE NOME DELLA TABELLA 
                 Column('id', Integer, primary_key = True),
-                Column('idMovie',None, ForeignKey('movies.id')),
-                Column('viewerName', String),
-                Column('viewerAge', String),
-                Column('numberOfSeat', Integer, nullable = False),
-                Column('clientUsername',None, ForeignKey('clients.id'))
+                Column('dateTime',DateTime),#NOT NULL
+                Column('price', Float),#NOT NULL >=0
+                Column('idMovie',None, ForeignKey('movies.id'), nullable = False),
+                Column('theater', None, ForeignKey('theaters.id'), nullable = False)
+                
               )
+booking = Table('booking', metadata, 
+                Column('id', Integer, primary_key = True),
+                Column('viewerName', String),#NOT NULL
+                Column('viewerAge', String),#NOT NULL
+                Column('seatNumber', Integer, nullable = False),
+                Column('clientUsername',None, ForeignKey('clients.id'), nullable = False),
+                Column('idmovieShedule', None, ForeignKey('movieShedule.id'), nullable = False)
+)
+
 metadata.create_all(engine)
 #----------fine tabella
